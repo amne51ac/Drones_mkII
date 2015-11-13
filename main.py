75630747f318e8fff14a262b741441d9338a7317
amne51ac <th3amne51ac@gmail.com>
2015-11-13 15:59:29 -0600

    continue the lords work

--- main.py
+++ main.py
@@ -6,10 +6,26 @@ Created on Fri Nov 13 14:56:33 2015
 """
 
 
+import dronekit
+
+
 class Drones():
 
     def __init__(self):
-            return
+        return
+
+    def connect(self):
+        return
+
+    def waypoint_handling(self):
+        return
+
+    def interface(self):
+        return
+
+    def mainloop(self):
+        return
+
 
 if __name__ == "__main__":
     print "This is the beginning of the drone adventure!"
