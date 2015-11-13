686a016ba7733ebbca262db094ae47f4a1f982ef
amne51ac <th3amne51ac@gmail.com>
2015-11-13 15:39:59 -0600

    added structure

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ Created on Fri Nov 13 14:56:33 2015
 @author: amne51ac
 """
 
+
+class Drones():
+
+    def __init__(self):
+            return
+
+if __name__ == "__main__":
+    print "This is the beginning of the drone adventure!"
