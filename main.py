881f1e0a1b3f10990d44e2527b8448eb1115bab7
amne51ac <th3amne51ac@gmail.com>
2015-11-13 21:14:56 -0600

    workin it.

--- main.py
+++ main.py
@@ -2,24 +2,28 @@
 """
 Created on Fri Nov 13 14:56:33 2015
 
-@author: amne51ac, Andrew Hessler
+@author: amne51ac, andrewhessloch
 """
 
 
 import dronekit
+import atexit
 
 
 class Drones():
 
     def __init__(self):
-        return
+        # actions to perform on start of program, you know what init does.
+
+        atexit.register(self.cleanup)
 
     def connect(self):
-        return
+        self.vehicle = dronekit.connect('127.0.0.1:14550', wait_ready=True)
 
     def waypoint_handling(self):
 
         def prepare_waypoint(self):
+            # self.vehicle.
             return
 
         def send_waypoint(self):
@@ -36,6 +40,15 @@ class Drones():
     def mainloop(self):
         return
 
+    def cleanup(self):
+        # actions to take at the end of the program, cleanup if you will lol
+        if self.vehicle:
+            self.vehicle.close()
+
 
 if __name__ == "__main__":
+    # This says that if it is the main program then execute this bit, otherwise
+    # it is probably being imported as a library, you know...
+
+    Drone = Drones()
     print "This is the beginning of the drone adventure!"
