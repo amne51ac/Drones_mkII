09ace5e5bf7f7f40e0ea53eb0449c3acdee1be08
amne51ac <th3amne51ac@gmail.com>
2015-11-13 22:23:51 -0600

    cranking

--- main.py
+++ main.py
@@ -10,17 +10,22 @@ import dronekit
 import atexit
 
 
-class Drones():
+class Delivery_Drones():
+    # delivery drones is a program for autonomous delivery quadcopter platforms
     waypoints = None
     address = None
+    cmds = None
 
     def __init__(self, address='127.0.0.1:14550'):
         # actions to perform on start of program, you know what init does.
         self.address = address
+        self.connect()
+        self.cmds = self.vehicle.commands
 
         atexit.register(self.cleanup)
 
     def connect(self):
+        # builds the connection between this machine and the copter itself
         self.output("Connecting to %s" % self.address)
         try:
             self.vehicle = dronekit.connect(self.address, wait_ready=True)
@@ -33,24 +38,33 @@ class Drones():
     def waypoint_handling(self):
 
         def prepare_waypoint(self):
+            # prepare the waypoint object for relaying to the drone
             # self.vehicle.
             return
 
         def send_waypoint(self):
+            # send the waypoint object to the drone
             return
 
         def street_waypoints(self):
+            # a function for utilizing google maps queries to facilitate drone
+            # navigation
             return
 
         return
 
     def interface(self):
+        # here we can construct the graphic interface for the users
         return
 
     def mainloop(self):
+        # this loop will help track heartbeat messages and such
+
         return
 
     def output(self, message):
+        # this allows us to use a standard formatting for messages and
+        # implement logging as well
         print message
 
     def cleanup(self):
@@ -65,5 +79,8 @@ if __name__ == "__main__":
     # This says that if it is the main program then execute this bit, otherwise
     # it is probably being imported as a library, you know...
 
-    Drone = Drones()
+    Drone = Delivery_Drones()
+
+    Drone.mainloop()
+
     print "This is the beginning of the drone adventure!"
